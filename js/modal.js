c2e10eb1a9d3c64da2046f12039eaf287f31f031
Chilllove43 <sadri.berrabah@gmail.com>
2025-10-23 03:54:21 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -9,3 +9,23 @@ function closeModal() {
   const modal = document.getElementById("modal");
   modal.style.display = "none";
 }
+
+<!-- Modale pour agrandir l'image -->
+<div id="modal" class="modal" onclick="closeModal()">
+  <span class="close">&times;</span>
+  <img class="modal-content" id="modal-img" />
+</div>
+
+<script>
+  function openModal(imgElement) {
+    const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modal-img");
+    modal.style.display = "block";
+    modalImg.src = imgElement.src;
+  }
+
+  function closeModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+  }
+</script>
