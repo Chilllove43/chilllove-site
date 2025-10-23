9711bdd534feeab7602bbbb08c2c299b320ae028
Chilllove43 <sadri.berrabah@gmail.com>
2025-10-23 05:01:25 +0200

    Create gallery.js

---
+++ asset/gallery.js
@@ -0,0 +1,14 @@
+// === Chill Love 43 💛 - Galerie modale ===
+
+function openModal(img) {
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal-img");
+
+  modal.style.display = "flex";
+  modalImg.src = img.src;
+}
+
+function closeModal() {
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
