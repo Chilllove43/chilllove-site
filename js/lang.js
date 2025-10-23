62b21f5fc9ddbe971eaab36799160ffb30f35b4e
Chilllove43 <sadri.berrabah@gmail.com>
2025-10-23 03:27:02 +0200

    Update lang.js

--- js/lang.js
+++ js/lang.js
@@ -1,17 +1,1 @@
-function switchLang(lang) {
-  const allTexts = document.querySelectorAll("[data-lang]");
-  allTexts.forEach(el => {
-    if (el.getAttribute("data-lang") === lang) {
-      el.style.display = "";
-    } else {
-      el.style.display = "none";
-    }
-  });
-  localStorage.setItem("lang", lang); // on garde le choix de l’utilisateur
-}
 
-// 🏁 Au chargement de la page : FR par défaut
-document.addEventListener("DOMContentLoaded", () => {
-  const savedLang = localStorage.getItem("lang") || "fr";
-  switchLang(savedLang);
-});
